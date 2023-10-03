049a6909d132bb4c3739549aa0489c47377f9f1b
Sam Lee <samlee.byu@gmail.com>
2023-10-02 18:46:40 -0600

    Fix bugs for long run scrape - Add method to start anywhere in the loop

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def save_data(data, save=False):
     
     if save:
         #Clean the data before saving it to the file
-        DATA["Officer"] = DATA["Citation"].apply(lambda x: re.search(r'P(\d)', x).group(1))
+        DATA["Officer"] = DATA["Citation"].apply(lambda x: re.search(r'P(\d+)', str(x)).group(1) if isinstance(x, str) and re.search(r'P(\d+)', str(x)) else None)
         DATA["Fine"] = DATA["Fine"].apply(lambda x: float(x.replace('$', '').replace(',', '')) if isinstance(x, str) else x)
         DATA["Residence"] = DATA["License Plate/Vin"].str.split().str[0]
         DATA["IssuedDate"] = pd.to_datetime(DATA["Issued"], format='%b %d, %Y %I:%M %p').dt.strftime('%Y-%m-%d')
@@ -55,6 +55,7 @@ def measure_time(start_time, s, threshold=10.1):
         if current >= threshold:
             counting = False
             s.flag = True
+            s.go()
         time.sleep(0.1)
 
 #Create the chrome driver that can be referred to globally
@@ -118,12 +119,19 @@ class Scraper():
         find_citation_btn = driver.find_elements(By.CLASS_NAME, "v-btn__content")[0]
         find_citation_btn.click()
     
-    @staticmethod
-    def send_keys(data={"officer": 1, "index": 0}):
+    def send_keys(self, data={"officer": 1, "index": 0}):
+        self._citation_loaded = False
         index = "0"*(5-len(str(data["index"])))+str(data["index"])
         key = f"P{data['officer']}-{index}"
         #print(key)
         
+        #Time how long it takes to load each citation
+        start_time = time.perf_counter()
+
+        # Create and start the thread for measuring time
+        time_thread = threading.Thread(target=measure_time, args=(start_time,self))
+        time_thread.start()
+
         #Search the key into the search bar
         
         #Sometimes dynamically loaded websites will spoof the identifiers of their web elements
@@ -149,14 +157,29 @@ class Scraper():
         )
         search_btn = driver.find_elements(By.CSS_SELECTOR, ".v-input__append-inner button")[0]
         search_btn.click()
+
+        self._citation_loaded = True
+        time_thread.join()
     
     def get_data(self):
+        #Time how long it takes to scrape each citation
+        start_time = time.perf_counter()
+        self._citation_loaded = False
+
+        # Create and start the thread for measuring time
+        time_thread = threading.Thread(target=measure_time, args=(start_time,self))
+        time_thread.start()
+
         citation_data = driver.find_elements(By.CSS_SELECTOR, ".v-card__text .col")
         no_data_text = driver.find_elements(By.CSS_SELECTOR, ".v-card__text .text-center h4")
         while not self.flag and len(citation_data) < 1 and len(no_data_text) < 1:
             citation_data = driver.find_elements(By.CSS_SELECTOR, ".v-card__text .col")
             no_data_text = driver.find_elements(By.CSS_SELECTOR, ".v-card__text .text-center h4")
             time.sleep(0.1)
+
+        self._citation_loaded = True
+        #Join the current thread
+        time_thread.join()
         
         if len(no_data_text) >= 1:
             return pd.DataFrame()
@@ -200,26 +223,26 @@ class Scraper():
         #Two columns to be merged with the row that scrapes the general information
         return pd.DataFrame.from_dict(citationInfo, orient='index').T
 
-    def main_loop(self):
+    def main_loop(self, i = 1, j = 0, start_at={"i": False, "j": False}):
         self.go()
         self.find_citation()
-        for i in self.officers:
-            j = 0
+        if not start_at["i"]:
+            i = 1
+        while i < len(self.officers):
+            start_at["i"] = False
+            if not start_at["j"]:
+                j = 0
+            no_data_sequence = 0
+            finished = False
             while j < len(self.index):
-                self._citation_loaded = False
+                start_time = time.perf_counter()
+                start_at["j"] = False
 
                 #Reset the flag:
                 if self._flag == True:
                     self._flag = False
                     self.go()
                     self.find_citation()
-
-                #Time how long it takes to scrape each citation
-                start_time = time.perf_counter()
-
-                # Create and start the thread for measuring time
-                time_thread = threading.Thread(target=measure_time, args=(start_time,self))
-                time_thread.start()
 
                 try:
                     self.send_keys({"officer": i, "index": j})
@@ -228,10 +251,14 @@ class Scraper():
                     #Save it to a file when it is the last index
                     #i.e. save it to a file when we are done scraping each officer's citations
                     scraped_data = self.get_data()
-                    if not scraped_data.empty:
-                        save_data(scraped_data, save = j == len(self.index)-1)
+                    if not scraped_data.empty:                      
+                        #Save to a file every 1000 rows scraped, or if it's the last index
+                        to_file = (j == len(self.index)-1) or j % 1000 == 0 or finished
+                        save_data(scraped_data, save = to_file)
+                        no_data_sequence = 0
                     else:
                         print("No data")
+                        no_data_sequence += 1
                     self._citation_loaded = True
                 except Exception as e:
                     now = datetime.datetime.now()
@@ -240,10 +267,8 @@ class Scraper():
                     error_message = f"{now}: {str(e)};\nThere was an error sending the keys or scraping the data: {key}"
                     with open("errors.txt", "a") as f:
                         f.write(error_message + "\n\n")
-                    #self._flag = True
+                    self._flag = True
 
-                #Join the current thread
-                time_thread.join()
                 print(f"Execution time: {time.perf_counter()-start_time:.6f}\n")
 
                 print("Most recent data scraped:")
@@ -259,6 +284,13 @@ class Scraper():
                 #Generally increase the jth index after each iteration
                 j += 1
 
+                if finished:
+                    break
+
+                if no_data_sequence >= 50 and abs(datetime.datetime.now() - datetime.datetime.strptime(DATA.tail(1)["Issued"].iloc[-1], '%b %d, %Y %I:%M %p')) <= datetime.timedelta(days=30):
+                    finished = True
+
+            i += 1
         print("Finished scraping all the data.")
         self.calculateTime()
 
@@ -275,4 +307,4 @@ if __name__ == "__main__":
         input("Press any key to continue...")
         user_input = input("Are you logged in yet? (y/n): ").rstrip().upper()
     
-    scraper.main_loop()
+    scraper.main_loop(i=1, j=13881, start_at={"i": True, "j": True})

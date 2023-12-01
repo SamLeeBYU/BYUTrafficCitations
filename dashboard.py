369cf6e49e50abb287768ab36d19a40ba0cb6085
Sam Lee <samlee.byu@gmail.com>
2023-12-01 02:24:47 -0700

    Create time series regression models

--- dashboard.py
+++ dashboard.py
@@ -2,7 +2,10 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 import plotly.express as px
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.linear_model import LinearRegression
+import itertools
+import plotly.graph_objects as go
 
 from sympy import sympify, symbols, sqrt
 
@@ -24,6 +27,7 @@ st.markdown("## Dashboard Guide")
 st.markdown("- [Key Metrics Over Time](#key-metrics-over-time)")
 st.markdown("- [Time Series Dependence Analysis](#lags-of-key-metrics)")
 st.markdown("- [Compare Different Metrics](#analyze-different-metrics-by-school-term)")
+st.markdown("- [Time Series Modeling](#the-model)")
 
 provo = pd.read_csv("Provo.csv")
 
@@ -180,3 +184,250 @@ term_metrics_fig.update_xaxes(title_text=f"{metric} {xlabel}")
 st.plotly_chart(term_metrics_fig)
 
 st.write(f"R-squared: {term_metrics[term_metric].corr(term_metrics[metric])**2}")
+
+################################################################################################################
+
+st.markdown("---")
+
+st.markdown("## The Model")
+
+st.markdown('''
+In the end I computed three main models to help me decipher the effect that environmental factors had on daily fluctuations in traffic citations:
+         
+1) A multilinear regression model using lasso variable selection methods. Due to time series dependency, a series of lag variable and lag interaction terms are also included in the regression.
+2) A multilinear regression using first differences to account for the time series dependence. A lasso regression was also used here to select the appropriate variables. Additionally, due to non-normality of the residuals, robust linear regression was performed.
+3) A random forest model across all factors for optimal prediction.
+            
+A more in depth analysis of the model fitting and selection process can be found on my blog post.
+         ''')
+
+provo = pd.read_csv("Provo.csv")
+provo["DATE"] = pd.to_datetime(provo["DATE"])
+
+recompile_models = st.button("Recompile Models")
+
+fileExists = False
+predictions = pd.DataFrame()
+
+try:
+    predictions = pd.read_csv("predictions.csv")
+    fileExists = True
+except Exception as e:
+    fileExists = False
+
+if not fileExists or recompile_models:
+    model_factors = ["MaxTemp", "MinTemp", "MeanTemp", "RainPrecip", "SnowPrecip",
+                    "Wind", "CO", "NO2", "O3", "PM10", "PM25", "AQI", "Mon", 
+                    "Tues", "Wed", "Thurs", "Fri", "Sun", "FullTime", 
+                    "Holiday", "Exam", "February", "March", "April", "May", 
+                    "June", "July", "August", "September", "October", "November",
+                    "December", "Spring", "Summer", "Fall", "DailyNumFines_Lag1",
+                    "DailyNumFines_Lag7",
+                    "DailyNumFines_Lag14", "DailyNumFines_Lag21", 
+                    "DailyNumFines_Lag28"]
+
+    lags = [1, 7, 14, 21, 28]
+
+    provo['log_DailyNumFines'] = np.log(provo['DailyNumFines'] + 1)
+    for lag in lags:
+        provo[f'DailyNumFines_Lag{lag}'] = provo['DailyNumFines'].shift(lag)
+
+    # Missing Data
+    end_date = "2019-04-23"
+    begin_date = "2017-03-08"
+
+    # COVID
+    covid_begin_date = "2020-03-14"
+    covid_end_date = "2020-09-07"
+
+    def truncate_provo(p):
+        return p[~((p['DATE'] >= pd.to_datetime(begin_date)) & (p['DATE'] <= pd.to_datetime(end_date)) |
+                (p['DATE'] >= pd.to_datetime(covid_begin_date)) & (p['DATE'] <= pd.to_datetime(covid_end_date)))]
+
+
+    # Add dummy variables
+    provo = pd.concat([provo, pd.get_dummies(provo["Term"])], axis=1)
+
+    log_factors = ["RainPrecip", "SnowPrecip", "Wind", "NO2", "PM10", "PM25", "AQI", "FullTime"]
+
+    # Log transformation for log factors
+    for factor in log_factors:
+        provo[factor] = np.log(provo[factor] + 1)
+
+    polynomial_factors = ["MaxTemp", "MinTemp", "MeanTemp", "RainPrecip", "SnowPrecip",
+                        "Wind", "CO", "NO2", "O3", "PM10", "PM25", "AQI",
+                        "FullTime", "DailyNumFines_Lag1",
+                        "DailyNumFines_Lag7",
+                        "DailyNumFines_Lag14", "DailyNumFines_Lag21", 
+                        "DailyNumFines_Lag28"]
+
+    for factor in polynomial_factors:
+        provo[f'{factor}_2'] = provo[factor]**2
+        
+    polynomial_factors = [f'{factor}_2' for factor in polynomial_factors]
+
+    env_lag_factors = []
+
+    def add_interaction_factors():
+        global provo
+        interaction_factors = []
+
+        env_factors = ["MaxTemp", "MinTemp", "MeanTemp", "RainPrecip", "SnowPrecip",
+                    "Wind", "CO", "NO2", "O3", "PM10", "PM25", "AQI"]
+
+        days = ["Mon", "Tues", "Wed", "Thurs", "Fri", "Sun"]
+
+        months = ["February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+        # Create dummy variables for months
+        for month in months:
+            col = provo['Month'] == month
+            col.name = month
+            provo = pd.concat([provo, col], axis=1)
+        seasons = ["Spring", "Summer", "Fall"]
+        lag_factors = ["DailyNumFines_Lag1", "DailyNumFines_Lag7", "DailyNumFines_Lag14", "DailyNumFines_Lag21", "DailyNumFines_Lag28"]
+
+        combinations = list(itertools.combinations(env_factors, 2))
+        for combination in combinations:
+            factor = '_'.join(combination)
+            interaction_factors.append(factor)
+            col = provo[combination[0]] * provo[combination[1]]
+            col.name = factor
+            provo = pd.concat([provo, col], axis=1)
+
+        for day in days:
+            factor = f'{day}_DailyNumFines_Lag1'
+            env_lag_factors.append(factor)
+            interaction_factors.append(factor)
+            col = provo[day] * provo['DailyNumFines_Lag1']
+            col.name = factor
+            provo = pd.concat([provo, col], axis=1)
+
+        for env_factor in env_factors:
+            for lag_factor in lag_factors:
+                factor = f'{env_factor}_{lag_factor}'
+                env_lag_factors.append(factor)
+                interaction_factors.append(factor)
+                col = provo[env_factor] * provo[lag_factor]
+                col.name = factor
+                provo = pd.concat([provo, col], axis=1)
+
+            for month in months:
+                factor = f'{env_factor}_{month}'
+                interaction_factors.append(factor)
+                col = provo[env_factor] * provo[month]
+                col.name = factor
+                provo = pd.concat([provo, col], axis=1)
+
+            for season in seasons:
+                factor = f'{env_factor}_{season}'
+                interaction_factors.append(factor)
+                col = provo[env_factor] * provo[season]
+                col.name = factor
+                provo = pd.concat([provo, col], axis=1)
+
+        return interaction_factors
+
+    interaction_factors = add_interaction_factors()
+
+    model_factors = model_factors + interaction_factors + polynomial_factors
+
+    provo_truncated = truncate_provo(provo)
+
+    lasso_factors = [
+        "NO2", "AQI", "CO", "SnowPrecip", "MinTemp", "MaxTemp", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sun", "FullTime", "Holiday", "Exam", "DailyNumFines_Lag1", "DailyNumFines_Lag14", "DailyNumFines_Lag21", "February", "May", "November", "Spring", "Fall", "RainPrecip", "December", "Wind", "DailyNumFines_Lag7", "August", "O3", "PM10", "DailyNumFines_Lag28", "PM25", "July",
+        "SnowPrecip_CO", "Mon_DailyNumFines_Lag1", "Tues_DailyNumFines_Lag1", "Wed_DailyNumFines_Lag1", "Thurs_DailyNumFines_Lag1", "Fri_DailyNumFines_Lag1", "Sun_DailyNumFines_Lag1",
+        "MinTemp_DailyNumFines_Lag1", "MaxTemp_DailyNumFines_Lag14", "MaxTemp_DailyNumFines_Lag21", "MinTemp_February", "MinTemp_May", "MinTemp_November", "MinTemp_Spring", "MinTemp_Fall", "RainPrecip_May", "RainPrecip_December", "SnowPrecip_DailyNumFines_Lag1", "SnowPrecip_DailyNumFines_Lag14", "SnowPrecip_DailyNumFines_Lag21",
+        "SnowPrecip_February", "Wind_DailyNumFines_Lag7", "Wind_DailyNumFines_Lag14", "Wind_Summer", "CO_August", "CO_November", "NO2_August", "O3_DailyNumFines_Lag7", "O3_DailyNumFines_Lag14", "O3_May", "PM10_DailyNumFines_Lag21", "PM10_DailyNumFines_Lag28", "PM25_July", "PM25_December", "CO_2", "FullTime_2", "DailyNumFines_Lag1_2", "DailyNumFines_Lag7_2"
+    ]
+
+    provo_lasso = model.fit(provo_truncated[lasso_factors].iloc[np.max(lags):], provo_truncated["log_DailyNumFines"].iloc[np.max(lags):])
+
+    lasso_df = pd.DataFrame({
+        "DATE": provo["DATE"].iloc[np.max(lags):].tolist(),
+        "Model": ["Multilinear (w/ Weekly Lags)"]*provo.iloc[np.max(lags):].shape[0],
+        "PredictedDailyNumFines": np.exp(provo_lasso.predict(provo[lasso_factors].iloc[np.max(lags):]))
+    })
+
+    difference = 1
+
+    difference_factors = [
+        "NO2", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sun", "Holiday", "Exam", "MaxTemp", "PM25", "MinTemp", "MeanTemp", "RainPrecip", "CO", "SnowPrecip", "Wind", "O3", "AQI", "October", "December", "June", "Wind", "April",  "May", "August", "Spring", "Summer", "PM10", "September", "February",
+        "MaxTemp_PM25", "MinTemp_MeanTemp", "RainPrecip_CO", "SnowPrecip_CO", "Wind_NO2", "NO2_O3", "PM25_AQI", "MeanTemp_October", "RainPrecip_December", "SnowPrecip_June", "Wind_April", "Wind_May", "CO_April", "CO_August", "CO_December", "CO_Spring", "NO2_August",
+        "NO2_Spring", "NO2_Summer", "PM10_April", "PM10_June", "PM10_September", "PM10_December", "AQI_February", "SnowPrecip_2"
+    ]
+
+    for factor in ["log_DailyNumFines"] + difference_factors:
+        col = provo[factor]-provo[factor].shift(difference)
+        col.name = factor + "_change"
+        provo_truncated = pd.concat([provo_truncated, col], axis=1)
+        provo = pd.concat([provo, col], axis=1)
+
+    lasso_df = lasso_df.merge(provo, how="right")
+
+    difference_factors = [factor + "_change" for factor in difference_factors]
+
+    provo_first_difference = model.fit(provo_truncated[difference_factors].iloc[difference:], provo_truncated["log_DailyNumFines_change"].iloc[difference:])
+
+    difference_df = pd.DataFrame({
+        "DATE": provo["DATE"].iloc[difference:].tolist(),
+        "Model": ["First Differences"]*provo.iloc[difference:].shape[0],
+        "PredictedDailyNumFines": np.exp(provo_first_difference.predict(provo[difference_factors].iloc[difference:]) + np.log(provo["DailyNumFines"].shift(difference) + 1).iloc[1:])
+    })
+
+    difference_df = difference_df.merge(provo, how="right")
+
+    rf = RandomForestRegressor(n_estimators=100, random_state=1120)
+    rf.fit(provo[model_factors].iloc[max(lags):], provo["log_DailyNumFines"].iloc[max(lags):])
+
+    rf_df = provo.merge(pd.DataFrame({
+        "DATE": provo["DATE"].iloc[max(lags):],
+        "Model": "Random Forest",
+        "PredictedDailyNumFines": np.exp(rf.predict(provo[model_factors].iloc[max(lags):]))
+    }), how="left")
+
+    predictions = pd.concat([lasso_df, difference_df, rf_df])
+    predictions.to_csv("predictions.csv", index=False)
+
+col1, col2 = st.columns(2)
+
+with col1:
+    selected_year = st.selectbox("Select a Year", ["All"]+(np.unique(provo["DATE"].dt.year)).tolist(), index=0)
+
+with col2:
+    selected_term = st.selectbox("Select a Term", ["All"]+(np.unique(provo["Term"]).tolist()), index=0)
+
+if selected_year == "All":
+    selected_year = np.unique(provo["DATE"].dt.year).tolist()
+else:
+    selected_year = [int(selected_year)]
+
+if selected_term == "All":
+    selected_term = np.unique(provo["Term"]).tolist()
+else:
+    selected_term = [selected_term]
+
+selected_models = predictions["Model"].dropna().unique()
+
+model_smoothness = st.slider("Adjust Smoothness", min_value=1, max_value=180, value=30)
+rolling_means = predictions[predictions["Model"].isin(selected_models)].copy()
+rolling_means["PredictedDailyNumFines"] = rolling_means.groupby("Model")["PredictedDailyNumFines"].rolling(window=model_smoothness).mean().reset_index(drop=True)
+rolling_means["DailyNumFines"] = rolling_means.groupby("Model")["DailyNumFines"].rolling(window=model_smoothness).mean().reset_index(drop=True)
+rolling_means = rolling_means.dropna()
+rolling_means["DATE"] = pd.to_datetime(rolling_means["DATE"])
+
+provo["DailyNumFines"] = provo["DailyNumFines"].rolling(window=model_smoothness).mean().reset_index(drop=True)
+provo["DailyNumFines"] = provo["DailyNumFines"].dropna()
+
+
+if rolling_means.empty:
+    st.write("Actual # of Daily Fines Over Times")
+    predictive_plot = px.line(provo, x="DATE", y="DailyNumFines", color_discrete_sequence=["#ff97ff"])
+    predictive_plot.update_yaxes(title_text="Daily # of Fines")
+    st.plotly_chart(predictive_plot)
+else:
+    for m in selected_models:
+        data = rolling_means[(rolling_means["Model"] == m) & (rolling_means["DATE"].dt.year.isin(selected_year)) & (rolling_means["Term"].isin(selected_term))].copy()
+        model_plot = px.line(data, x="DATE", y="PredictedDailyNumFines", color="Model")
+        model_plot.add_scatter(x=data["DATE"], y=data["DailyNumFines"], mode='lines', name='Actual', line=dict(color='#ff97ff'))
+        model_plot.update_yaxes(title_text="Daily # of Fines")
+        st.plotly_chart(model_plot)
